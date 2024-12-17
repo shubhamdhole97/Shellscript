41744be42c859f1d901107450b185fb7df1d90a3
shubhamdhole97 <shubhamdhole97@gmail.com>
2024-12-17 11:12:05 +0000

    Initial Commit

---
+++ lengthofstringfromuser.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Prompt the user for input
+read -p "Enter a string: " user_input
+
+# Calculate and display the length of the input string
+echo "Length of the entered string is ${#user_input}"
+
